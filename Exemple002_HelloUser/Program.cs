ec41c2286030f87795760118d0f47aba0f018e8a
Sergey <sergeypalii@gmail.com>
2023-04-20 20:54:13 +0500

    user_Masha

--- Exemple002_HelloUser/Program.cs
+++ Exemple002_HelloUser/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.Write("Введите ваше имя");
-string username = Console.ReadLine();
-Console.Write("Привет,");
-Console.Write(username);
+﻿Console.WriteLine("Введите ваше имя");
+string username=Console.ReadLine();
+if (username=="Маша")
+{
+    Console.WriteLine("Ура, это же наша Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
